34f8b3189964a1c15446f56a8823c6886981c34a
tharathepkularb <tharathep.ku@ku.th>
2020-10-03 20:50:59 +0700

    committtt

--- src/main/java/th/ac/ku/atm/model/Customer.java
+++ src/main/java/th/ac/ku/atm/model/Customer.java
@@ -43,6 +43,16 @@ public class Customer {
     public void setPin(String pin) {
         this.pin = pin;
     }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pin='" + pin + '\'' +
+                '}';
+    }
+
 }
 
 
