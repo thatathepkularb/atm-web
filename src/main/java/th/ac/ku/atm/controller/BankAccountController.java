13a3ac61fa704746b53a3021e2939f1b294a09c2
tharathepkularb <tharathep.ku@ku.th>
2020-10-03 21:21:51 +0700

    edit bankaccount

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -1,10 +1,7 @@
 package th.ac.ku.atm.controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 import th.ac.ku.atm.model.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
@@ -29,8 +26,27 @@ public class BankAccountController {
     @PostMapping
     public String openAccount(@ModelAttribute BankAccount bankAccount, Model model) {
         accountService.openAccount(bankAccount);
-        model.addAttribute("bankaccounts",accountService.getBankAccounts());
+        model.addAttribute("bankaccounts", accountService.getBankAccounts());
         return "redirect:bankaccount";
     }
+
+    @GetMapping("/edit/{id}")
+    public String getEditBankAccountPage(@PathVariable int id, Model model) {
+        BankAccount account = accountService.getBankAccount(id);
+        model.addAttribute("bankAccount", account);
+        return "bankaccount-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editAccount(@PathVariable int id,
+                              @ModelAttribute BankAccount bankAccount,
+                              Model model) {
+
+        accountService.editBankAccount(bankAccount);
+        model.addAttribute("bankaccounts",accountService.getBankAccounts());
+        return "redirect:/bankaccount";
+    }
+
+
 }
 
--- src/main/java/th/ac/ku/atm/service/BankAccountService.java
+++ src/main/java/th/ac/ku/atm/service/BankAccountService.java
@@ -35,6 +35,22 @@ public class BankAccountService {
         restTemplate.postForObject(url, bankAccount, BankAccount.class);
     }
 
+    public BankAccount getBankAccount(int id) {
+        String url = "http://localhost:8091/api/bankaccount/" + id;
+
+        ResponseEntity<BankAccount> response =
+                restTemplate.getForEntity(url, BankAccount.class);
+
+        return response.getBody();
+    }
+
+    public void editBankAccount(BankAccount bankAccount) {
+        String url = "http://localhost:8091/api/bankaccount/" +
+                bankAccount.getId();
+        restTemplate.put(url, bankAccount);
+    }
+
+
 
 
     public void createBankAccount(BankAccount bankAccount) {
